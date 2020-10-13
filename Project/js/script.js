31304653749fe8a9c5bd66b2c9bc409d24a5a60f
Sergey <s.yakimaha78@gmail.com>
2020-10-13 11:04:34 +0300

    Lesson 38

--- Project/js/script.js
+++ Project/js/script.js
@@ -1,97 +1,48 @@
-/* Задания на урок:
+window.addEventListener('DOMContentLoaded', function() {
 
-1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
-новый фильм добавляется в список. Страница не должна перезагружаться.
-Новый фильм должен добавляться в movieDB.movies.
-Для получения доступа к значению input - обращаемся к нему как input.value;
-P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+    // Tabs
+    
+	let tabs = document.querySelectorAll('.tabheader__item'),
+		tabsContent = document.querySelectorAll('.tabcontent'),
+		tabsParent = document.querySelector('.tabheader__items');
 
-2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
-
-3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
-
-4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
-"Добавляем любимый фильм"
-
-5) Фильмы должны быть отсортированы по алфавиту */
-
-'use strict';
-
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
-
-const adv = document.querySelectorAll('.promo__adv img'),
-      poster = document.querySelector('.promo__bg'),
-      genre = poster.querySelector('.promo__genre'),
-      movieList = document.querySelector('.promo__interactive-list'),
-      formAdd = document.querySelector('.add');
-
-
-adv.forEach(item => {
-    item.remove();
-});
-
-genre.textContent = 'драма';
-
-poster.style.backgroundImage = 'url("img/bg.jpg")';
-
-fillFilmsList();
-
-function fillFilmsList() {
-    movieList.innerHTML = "";
-
-    movieDB.movies.forEach((film, i) => {
+	function hideTabContent() {
         
-        movieList.innerHTML += `
-            <li class="promo__interactive-item">${i + 1} ${film}
-                <div class="delete" index=${i}></div>
-            </li>
-        `;
-    }); 
-    
-    addEventsDelete();
-}
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
 
-function addEventsDelete() {
-    movieList.querySelectorAll('.delete').forEach(function(elem) {
-        elem.addEventListener('click', deleteFilm);
-    });     
-}
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+	}
 
-formAdd.querySelector('button').addEventListener('click', addFilm);
-formAdd.querySelector('input[type="checkbox"').addEventListener('click', likeFilm);
-
-function addFilm(event) {
-    event.preventDefault();
-    
-    const input = formAdd.querySelector('input');
-    
-    let filmName = (input.value.length > 21) ? input.value.slice(0, 21) + '...' : input.value;
-
-    movieDB.movies.push(filmName);
-    movieDB.movies.sort();
-
-    fillFilmsList();
-}
-
-function deleteFilm(event) {
-    let index = event.target.getAttribute('index');
-    
-    movieDB.movies.splice(index, 1);
-    movieDB.movies.sort(); 
-    
-    fillFilmsList();
-}
-
-function likeFilm(event) {
-    if (event.target.checked) {
-        console.log('Добавляем любимый фильм');
+	function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
     }
-}
+    
+    hideTabContent();
+    showTabContent();
+
+	// tabsParent.addEventListener('click', function(event) {
+	// 	const target = event.target;
+	// 	if(target && target.classList.contains('tabheader__item')) {
+    //         tabs.forEach((item, i) => {
+    //             if (target == item) {
+    //                 hideTabContent();
+    //                 showTabContent(i);
+    //             }
+    //         });
+	// 	}
+    // });
+    
+    tabs.forEach((item, i) => {
+        item.addEventListener('click', function() {
+            hideTabContent();
+            showTabContent(i);
+        });
+    });    
+});
